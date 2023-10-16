828e3f337367a58fdac1848630cfde0402346737
TheTRanceMachine <grzegorz.smolin2@gmail.com>
2023-10-16 17:11:38 -0400

    User Management - Login, Logout, Registration

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -9,6 +9,9 @@ urlpatterns = [
     # view refers to the view function
     # name the URL
 
+    # path for index
+    path(route='', view=views.get_dealerships, name='index'),
+
     # path for about view
     path(route='about', view=views.about, name='about'),
 
@@ -16,12 +19,13 @@ urlpatterns = [
     path(route='contact', view=views.contact, name='contact'),
 
     # path for registration
+    path(route='registration', view=views.registration_request, name='registration'),
 
     # path for login
+    path(route='login', view=views.login_request, name='login'),
 
     # path for logout
-
-    path(route='', view=views.get_dealerships, name='index'),
+    path(route='logout', view=views.logout_request, name='logout'),
 
     # path for dealer reviews view
 
