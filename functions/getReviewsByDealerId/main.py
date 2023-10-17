7351d5118a2c17cad4346098725f704c7ce93ba7
TheTranceMachine <grzegorz.smolin2@gmail.com>
2023-10-17 14:13:46 -0400

    Created functions node.js and python - part1

---
+++ functions/getReviewsByDealerId/main.py
@@ -0,0 +1,50 @@
+"""IBM Cloud Function that gets all reviews by dealer ID
+Returns:
+    List: List of reviews by Dealer ID
+"""
+from ibm_cloud_sdk_core import ApiException
+from ibmcloudant.cloudant_v1 import CloudantV1
+import requests
+
+
+def main(param_dict):
+    """Main Function
+
+    Args:
+        param_dict (Dict): input paramater
+
+    Returns:
+        _type_: _description_ TODO
+    """
+
+    try:
+        fields = [
+            'id',
+            'name',
+            'dealership',
+            'review',
+            'purchase',
+            'purchase_date',
+            'car_make',
+            'car_model',
+            'car_year'
+        ]
+        client = CloudantV1.new_instance()
+        response = client.post_find(
+            db='reviews',
+            selector={'dealership': {'$eq': param_dict["DEALER_ID"]}},
+            fields=fields
+        ).get_result()
+
+        return {"dbs": response}
+    except ApiException as cloudant_exception:
+        if str(cloudant_exception.code) == "404":
+            return {"error": "dealerId does not exist"}
+        elif str(cloudant_exception.code) == "500":
+            return {"error": "Something went wrong on the server"}
+        else:
+            return {"error": "status code: " + str(cloudant_exception.code) 
+            + " error message: " + cloudant_exception.message}
+    except (requests.exceptions.RequestException, ConnectionResetError) as err:
+        print("connection error")
+        return {"error": err}
